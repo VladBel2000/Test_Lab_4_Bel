9c9d17fba551453fc4c0d9c0dbe413f6a0c9e97d
VladBel2000 <vlad.belomestnov.00@mail.ru>
2022-04-03 16:45:33 +0300

    Создан начальный метод генерации пароля

--- Test_Lab_4/Program.cs
+++ Test_Lab_4/Program.cs
@@ -9,16 +9,21 @@ namespace Test_Lab_4
             String answer;
             int count_users;
 
-            public leader(String p_answer, int p_count_users)
+            public leader(int p_count_users)
             {
-                answer = p_answer;
+                answer = generate_answer();
                 count_users = p_count_users;
+            }
+            public string generate_answer()
+            {
+                String now_password = "Ответ";
+                return now_password;
             }
 
         }
         static void Main(string[] args)
         {
-            leader now_leader = new leader("Ответ", 4);
+            leader now_leader = new leader(4);
         }
     }
 }
