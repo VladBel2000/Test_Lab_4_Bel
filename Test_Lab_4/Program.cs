d3b7b5ddd2028d98fcbce584917b43335362232c
VladBel2000 <vlad.belomestnov.00@mail.ru>
2022-04-03 16:42:27 +0300

    Инициализация класса ведущий

--- Test_Lab_4/Program.cs
+++ Test_Lab_4/Program.cs
@@ -4,9 +4,21 @@ namespace Test_Lab_4
 {
     class Program
     {
+        class leader
+        {
+            String answer;
+            int count_users;
+
+            public leader(String p_answer, int p_count_users)
+            {
+                answer = p_answer;
+                count_users = p_count_users;
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            leader now_leader = new leader("Ответ", 4);
         }
     }
 }
