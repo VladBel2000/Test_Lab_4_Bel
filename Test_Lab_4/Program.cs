0ee59fbb9bbf090d107ceb11e7a93fa7cf4d6f5b
VladBel2000 <vlad.belomestnov.00@mail.ru>
2022-04-03 17:17:27 +0300

    Добавлена функция создания зашифрованной строки

--- Test_Lab_4/Program.cs
+++ Test_Lab_4/Program.cs
@@ -8,12 +8,16 @@ namespace Test_Lab_4
         class leader
         {
             String answer;
+            String now_answer;
             int count_users;
             int now_user;
 
             public leader()
             {
                 answer = generate_answer();
+                answer = "Папа";
+
+                now_answer = generate_now_answer(answer.Length);
 
                 //Создание объекта для генерации чисел
                 Random rnd = new Random();
@@ -37,13 +41,18 @@ namespace Test_Lab_4
                 return now_user;
             }
 
-            //
-            public String get_print_answer()
-            {
 
-                return answer;
+            public string generate_now_answer(int count_char)
+            {
+                return new String('*', count_char);
+
             }
 
+            public String get_now_answer()
+            {
+
+                return now_answer;
+            }
 
             public string generate_answer()
             {
@@ -68,9 +77,10 @@ namespace Test_Lab_4
         static void Main(string[] args)
         {
             leader now_leader = new leader();
-            Console.WriteLine("Загаданное слово: " + now_leader.get_print_answer());
+            Console.WriteLine("Загаданное слово: " + now_leader.get_now_answer());
 
             Console.WriteLine("Игрок №" + now_leader.get_now_user() + ", введите букву:");
+            
         }
     }
 }
